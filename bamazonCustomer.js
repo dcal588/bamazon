393c168587a59237f6fb5be2fc3a5ef9664e4598
David Calderon <David@DESKTOP-7UAHA94.(none)>
2017-09-18 22:57:52 -0700

    Added Functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,7 +11,29 @@ var connection = mysql.createConnection({
     database: 'bamazon',
 });
 connection.query("SELECT * FROM products", function(err, res) {
-			for (var i = 0; i < res.length; i++) {
-				console.log("Item ID: "+res[i].item_id+ " | Product Name: " +res[i].product_name + " | Price: " + res[i].price);
-			}
+	for (var i = 0; i < res.length; i++) {
+		console.log("Item ID: "+res[i].item_id+ " | Product Name: " +res[i].product_name + " | Price: " + res[i].price);
+	}
+	chooseItem(res);
+});
+
+function chooseItem (res) {
+	inquirer.prompt([
+  	{
+  		type: "input",
+    	name: "itemchoice",
+    	message: "Please enter the ID of the item you would like to purchase."
+  	},
+  	{
+  		type: "input",
+    	name: "amount",
+    	message: "How many would you like?"
+  	}	
+  ])
+  .then(function(inquirerResponse) {
+  	var query = "SELECT item_id, product_name, price FROM products WHERE ?";
+    connection.query(query, { item_id: inquirerResponse.itemchoice }, function(err, res) {
+      	console.log("smile check");
 		});
+	});
+};
